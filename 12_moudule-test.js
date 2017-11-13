58175abb609f010bc3fb6b069a998a207a7699f6
mashubin <32285084+mashubin@users.noreply.github.com>
2017-11-13 16:07:33 +0800

    new ~

--- 12_moudule-test.js
+++ 12_moudule-test.js
@@ -1,5 +1,7 @@
 /**
  * Created by web-01 on 2017/11/13.
  */
-let m2=require('./m2/m2');
-console.log(m2.s2)
+// let m2=require('./m2/m2');
+// console.log(m2.s2)
+let m22 =require('./m2')
+console.log(m22.m22)
